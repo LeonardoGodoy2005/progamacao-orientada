118b698fb8d6cb562a31f47f403dd59580ac1248
Leonardo Godoy <leonardoghodoy2005@gmail.com>
2025-09-03 12:26:50 -0300

    progamação orientada

---
+++ sistema_radar/Carro.java
@@ -0,0 +1,57 @@
+package sistema_radar;
+
+public class Carro {
+   private String placa;
+   private Integer velocidade;
+   private Integer ano;
+   private String modelo;
+
+   public Carro(String placa, Integer velocidade, Integer ano, String modelo){
+    this.placa = placa;
+    this.velocidade = velocidade;
+    this.ano = ano;
+    this.modelo = modelo;
+   }
+   
+   public Integer getVelocidade(){
+        return this.velocidade;
+   }
+
+   public void setVelocidade(Integer velocidade){
+    if(velocidade != null && velocidade > 0){
+        this.velocidade = velocidade;
+    }
+   }
+   
+   public Integer getAno() {
+    return ano;
+}
+
+public void setAno(Integer ano) {
+    this.ano = ano;
+}
+
+public String getModelo() {
+    return modelo;
+}
+
+public void setModelo(String modelo) {
+    this.modelo = modelo;
+}
+
+
+   public String getPlaca(){
+    return this.placa;
+   }
+
+   public void acelerar(){
+    this.velocidade += 10;
+   }
+
+   public void frear(){
+    if(this.velocidade > 0){
+        this.velocidade -= 10;
+    }
+   }
+   
+}
